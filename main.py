07569936c8827d8b6e04ba447fc780813e2c71bc
kowalks <guilhermekowalczuk@gmail.com>
2020-08-10 18:13:49 -0300

    final release

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from script import *
 from pdfManipulation import *
 
 print("-- Sistema de Acesso --\n")
-#user = input(" Usuário: ")
-#pwd = getpass.getpass(" Senha: ")
+user = input(" Usuário: ")
+pwd = getpass.getpass(" Senha: ")
 
-#get_pdf(user, pwd)
+get_pdf(user, pwd)
 pdf_print()
 
